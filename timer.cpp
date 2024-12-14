5e4edb08a7f635d75cd18493f96b9d7098fbe3ce
pratri <pranteja@gmail.com>
2024-12-14 11:55:43 -0500

    kkk

--- timer.cpp
+++ timer.cpp
@@ -1,5 +1,17 @@
 #include "timer.h"
+// #include <chrono>
 
-Timer::Timer(QObject *parent)
-    : QObject{parent}
-{}
+
+Timer::Timer(QObject *parent) : QObject(parent)
+{
+}
+
+void Timer::userName()
+{
+
+}
+
+void Timer::setUserName(const QString &userName)
+{
+
+}
--- timer.h
+++ timer.h
@@ -4,25 +4,33 @@
 #include <QObject>
 #include <QString>
 #include <qqml.h>
+#include <QTime>
+#include <QTimer>
 
 class Timer : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QString userName READ userName WRITE setUserName NOTIFY userNameChanged)
+    // Q_PROPERTY(QString userName READ userName WRITE setUserName NOTIFY userNameChanged)
     QML_ELEMENT
 
 public:
     explicit Timer(QObject *parent = nullptr);
-    QString userName();
+    void userName();
     void setUserName(const QString &userName);
 
 signals:
     void userNameChanged();
 
 private:
-    QString m_userName;
-}
+    QTimer *timer;
+    QTime *stopwatchTime;
+    bool isRunning;
+    int elapsedTime;
+};
 
+
+// bolt.new and replit.agent (AI coding) liam.ottley video on AI agents.
+//chatp gpt relevance for(ai agents), zapier and make.com, promptmetheus (prompts), postman (api integraitons)
 
 #endif // TIMER_H
 
